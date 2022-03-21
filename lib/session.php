3cc983edb7a21aaa0f19127e4d878c4c029818ae
Iqbal Hossen <jmiqbal7265@gmail.com>
2022-03-21 23:03:10 +0600

    Simple CRM

--- lib/session.php
+++ lib/session.php
@@ -13,26 +13,4 @@ if(!isset($_SESSION['email'])){
     die();
 }
 
-$email = $_SESSION['email'];
-$db->where('email', $email);
-$user = $db->getOne('users');
 
-$branch = $db->get('branches');
-
-$total_branch = $db->getValue('branches','count(*)');
-$total_parcel = $db->getValue('parcels', 'count(*)');
-$db->where('type', 2);
-$total_staff = $db->getValue('users', 'count(*)');
-
-$db->where('status',0);
-$item_accepted_by_courier = $db->getValue('parcels','count(*)');
-$db->where('status',1);
-$collected = $db->getValue('parcels','count(*)');
-$db->where('status',2);
-$shipped = $db->getValue('parcels','count(*)');
-$db->where('status',7);
-$delivery = $db->getValue('parcels','count(*)');
-$db->where('status',8);
-$pickup = $db->getValue('parcels','count(*)');
-
-$setting = $db->getOne('system_settings');
